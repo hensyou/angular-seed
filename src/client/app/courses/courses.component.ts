6df1a5da69a33e6c795acfdc8146b7ce1d990362
Xiao Bian <xiaobian@wkstncontrols-MacBook-Pro-187.local>
2017-07-20 21:36:54 -0400

    update courses

--- src/client/app/courses/courses.component.ts
+++ src/client/app/courses/courses.component.ts
@@ -8,18 +8,43 @@ import { CourseService } from './course.service';
 
 })
 export class CoursesComponent implements OnInit {
-    title = 'the title of courses';
+    title :string;
     courses: any[] = [];
     errorMessage: string;
+    address :Address;
+    showAddress: boolean;
     constructor( public courseService : CourseService) {}
     ngOnInit() {
         this.getCourses();
+
+        this.address = {
+            state: 'NY',
+            city: 'New York',
+            street: 'Hartland St'
+        };
+        this.showAddress=false;
     }
 
     getCourses() {
-         this.courseService.get().subscribe(
+        this.title = 'Xiao Bian courses';
+        this.courseService.get().subscribe(
         courses => this.courses = courses,
         error => this.errorMessage = <any>error);
   }
+    toggleAddress() {
+        this.showAddress=!this.showAddress;
+    }
+    addCourse(course:string) {
+        console.log(course);
+        this.courses.push(course);
+        return false;
+    }
+
+}
+
+interface Address {
+    street:string;
+    city:string;
+    state:string;
 }
 
